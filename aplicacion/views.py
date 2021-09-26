90f6344a6217f88b6dfe538369ddd67f8c7005a7
CMJesus <67258065+CMJesus@users.noreply.github.com>
2021-09-27 00:03:12 +0200

    00:03

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,10 +1,23 @@
 from aplicacion import app
+from flask import render_template
+from aplicacion.models import DBManager
+
+ruta_basedatos = app.config.get("RUTA_BASE_DE_DATOS")
+dbManager = DBManager(ruta_basedatos)
 
 #Rutas
 
 @app.route("/")
 def inicio():
-    return "Página de Inicio"
+
+    consulta = """
+        SELECT * 
+            FROM movimientos 
+            ORDER BY fecha;"
+    """
+    movimientos = dbManager.consultaSQL(consulta)
+    
+    return render_template("index.html", items=movimientos)
 
 @app.route("/nuevo", methods=["GET", "POST"])
 def nuevo():

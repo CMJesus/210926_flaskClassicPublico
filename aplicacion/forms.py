9eb0ff19e4ec95379dd890c71da3a1f5b7041d2e
CMJesus <67258065+CMJesus@users.noreply.github.com>
2021-09-28 22:43:43 +0200

    210928_primera JavaScript

--- aplicacion/forms.py
+++ aplicacion/forms.py
@@ -1,13 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, StringField, FloatField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms import HiddenField, DateField, StringField, FloatField, RadioField, SubmitField
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+
+import datetime
+
+def validar_fecha(formulario, campo):
+    hoy = datetime.datime.today()
+    if campo.data > hoy:
+        raise ValidationError("La fecha no puede ser posterior a hoy")
 
 #Herencia desde librería, mucha funcionalidad
 class MovimientoFormulario(FlaskForm):
-    fecha = DateField("Fecha", validators=[DataRequired(message="Debe de informar la fecha")])
+
+    id = HiddenField()
+    
+    fecha = DateField("Fecha", validators=[DataRequired(message="Debe de informar la fecha"), validar_fecha])
     concepto = StringField("Concepto", validators=[DataRequired(message="Debe de informar el concepto"), Length(min=10)])
     cantidad = FloatField("Cantidad", validators=[DataRequired(message="Debe de informar una cantidad"), 
                                                     NumberRange(message="Debe de informar un importe positivo", min=0.01)])
     ingreso_gasto = RadioField(validators=[DataRequired(message="Debe de informar tipo de movimiento")], 
                                             choices=[('G', 'Gasto'), ('I', 'Ingreso')])
     submit = SubmitField('Aceptar')
+
+    def vakudate_fecha(self, campo):
+        hoy = datetime.datime.today()
+        if campo.data > hoy:
+            raise ValidationError("La fecha no puede ser posterior a hoy")
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -2,6 +2,7 @@ from aplicacion import app
 from flask import render_template, request, redirect, url_for
 from aplicacion.models import DBManager
 from aplicacion.forms import MovimientoFormulario
+from datetime import date
 
 ruta_basedatos = app.config.get("RUTA_BASE_DE_DATOS")
 dbManager = DBManager(ruta_basedatos)
@@ -31,7 +32,12 @@ def nuevo():
             consulta = """INSERT INTO movimientos (fecha, concepto, ingreso_gasto, cantidad)
                             VALUES (:fecha, :concepto, :ingreso_gasto, :cantidad)"""
 
-            dbManager.insertaSQL(consulta, formulario.data)
+            try:
+                dbManager.insertaSQL(consulta, formulario.data)
+            except Exception as es:
+                print("Se ha producido un error de acceso a base de datos:", e)
+                flash("Se ha producido un error en la base de datos. Consulte con su administrador")
+                return render_template("nuevo_mov.html", el_formulario=formulario)
 
             return redirect(url_for("inicio"))
         else:
@@ -45,4 +51,43 @@ def nuevo():
 
 @app.route("/borrar/<int:id>", methods=["GET", "POST"])
 def borrar(id):
-    return f"Página de borrado de {id}"
+    if request.method == 'GET':
+        consulta = """
+        SELECT id, fecha, concepto, ingreso_gasto, cantidad
+        FROM movimientos
+        WHERE id = ?;
+        """
+
+        movimientos = dbManager.consultaSQL(consulta, [id])
+        if len(movimientos) == 0:
+            flash(f"Movimiento {id} no encontrado")
+            return redirect(url_for("inicio"))
+
+
+        el_movimiento = movimientos[0]
+        el_movimiento["fecha"] = date.fromisoformat(el_movimiento["fecha"])
+        formulario = MovimientoFormulario(data=el_movimiento)
+            
+
+        return render_template("delete_mov.html", form=formulario, el_id=el_movimiento['id'])
+    
+    else:
+        pass
+    #Aquí hay que hacer lo mismo que en la consulta que tenemos, aslvo que con delete
+        # consulta = """INSERT INTO movimientos (fecha, concepto, ingreso_gasto, cantidad)
+        #                     VALUES (:fecha, :concepto, :ingreso_gasto, :cantidad)"""
+
+        #     try:
+        #         dbManager.insertaSQL(consulta, formulario.data)
+        #     except Exception as es:
+        #         print("Se ha producido un error de acceso a base de datos:", e)
+        #         flash("Se ha producido un error en la base de datos. Consulte con su administrador")
+        #         return render_template("nuevo_mov.html", el_formulario=formulario)
+
+        #     return redirect(url_for("inicio"))
+        # else:
+        #     return render_template("nuevo_mov.html", form = formulario)
+
+    
+    return 'Hola, soy un post'
+
